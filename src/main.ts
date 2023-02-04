79820fc6db2c668ce57fe937fe45ae5556602bf9
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-02-03 19:34:57 -0800

    Replaced forEaches with for loops

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 document.addEventListener("keydown", (ev) => {
   let sum = 0;
-  if (ev.shiftKey) {
-    sum += 1;
-  }
-  if (ev.ctrlKey) {
-    sum += 2;
-  }
-  if (ev.altKey) {
-    sum += 4;
-  }
-  const code = ev.code;
+  if (ev.shiftKey) sum += 1;
+  if (ev.ctrlKey) sum += 2;
+  if (ev.altKey) sum += 4;
+  const key = ev.code;
   browser.runtime.sendMessage({
-    key: `${code.search("Key") + code.search("Digit") === -1 ? code.at(-1) : code
+    key: `${key.search("Key") + key.search("Digit") === -1 ? key.at(-1) : key
       }${sum}`,
   });
 });
