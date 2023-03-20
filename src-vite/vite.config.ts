b788e12864a5257f207991cdf4dba5bad0a38d1c
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-20 11:48:12 -0700

    Setup build for vite

--- src-vite/vite.config.ts
+++ src-vite/vite.config.ts
@@ -4,4 +4,8 @@ import solid from "vite-plugin-solid";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [solid()],
+  build: {
+    emptyOutDir: false,
+    outDir: "../dist",
+  },
 });
