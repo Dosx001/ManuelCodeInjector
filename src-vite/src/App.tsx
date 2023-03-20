fed605ddcd7859429e8701f1535e7f375cd45c71
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-20 16:31:59 -0700

    Added a polyfill

--- src-vite/src/App.tsx
+++ src-vite/src/App.tsx
@@ -1,8 +1,19 @@
-import type { Component } from "solid-js";
+import { Component, createSignal, onMount } from "solid-js";
+import browser from "webextension-polyfill";
 import "./styles.scss";
 
 const App: Component = () => {
-  return <div class="">hello</div>;
+  const [bytes, setBytes] = createSignal("0 kB");
+  onMount(async () => {
+    setBytes(
+      `${(102400 - (await browser.storage.sync.getBytesInUse())) / 1000} kB`
+    );
+  });
+  return (
+    <>
+      <div>Remaining online storage: {bytes()}</div>
+    </>
+  );
 };
 
 export default App;
