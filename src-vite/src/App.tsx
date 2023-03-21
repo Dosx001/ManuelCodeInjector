28bfea6d59bd0da885d9e889d48989ba1f5ad859
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-20 17:51:27 -0700

    Created Row component

--- src-vite/src/App.tsx
+++ src-vite/src/App.tsx
@@ -1,5 +1,6 @@
 import { Component, createSignal, onMount } from "solid-js";
 import browser from "webextension-polyfill";
+import Row from "./components/Row";
 import "./styles.scss";
 
 const App: Component = () => {
@@ -31,93 +32,7 @@ const App: Component = () => {
               <th>Size</th>
               <th>Script</th>
             </tr>
-            <tr id="clone">
-              <td>
-                <button type="button" id="submit">
-                  Add
-                </button>
-              </td>
-              <td>
-                <select class="key" autocomplete="off">
-                  <option value="A">A</option>
-                  <option value="B">B</option>
-                  <option value="C">C</option>
-                  <option value="D">D</option>
-                  <option value="E">E</option>
-                  <option value="F">F</option>
-                  <option value="G">G</option>
-                  <option value="H">H</option>
-                  <option value="I">I</option>
-                  <option value="J">J</option>
-                  <option value="K">K</option>
-                  <option value="L">L</option>
-                  <option value="M">M</option>
-                  <option value="N">N</option>
-                  <option value="O">O</option>
-                  <option value="P">P</option>
-                  <option value="Q">Q</option>
-                  <option value="R">R</option>
-                  <option value="S">S</option>
-                  <option value="T">T</option>
-                  <option value="U">U</option>
-                  <option value="V">V</option>
-                  <option value="W">W</option>
-                  <option value="X">X</option>
-                  <option value="Y">Y</option>
-                  <option value="Z">Z</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="0">0</option>
-                  <option value="F1">F1</option>
-                  <option value="F2">F2</option>
-                  <option value="F3">F3</option>
-                  <option value="F4">F4</option>
-                  <option value="F5">F5</option>
-                  <option value="F6">F6</option>
-                  <option value="F7">F7</option>
-                  <option value="F8">F8</option>
-                  <option value="F9">F9</option>
-                  <option value="F10">F10</option>
-                  <option value="F11">F11</option>
-                  <option value="F12">F12</option>
-                </select>
-              </td>
-              <td class="mods">
-                <div>
-                  <input type="checkbox" autocomplete="off" />
-                  Shift
-                </div>
-                <div>
-                  <input type="checkbox" autocomplete="off" />
-                  Ctrl
-                </div>
-                <div>
-                  <input type="checkbox" autocomplete="off" />
-                  Alt
-                </div>
-              </td>
-              <td>
-                <input class="sync" type="checkbox" autocomplete="off" />
-              </td>
-              <td class="size">-</td>
-              <td>
-                <textarea
-                  class="code"
-                  cols="40"
-                  rows="5"
-                  placeholder="Type code here"
-                  autocomplete="off"
-                  spellcheck={false}
-                />
-              </td>
-            </tr>
+            <Row key="" />
           </thead>
           <tbody />
         </table>
---
+++ src-vite/src/components/Row.tsx
@@ -0,0 +1,100 @@
+const Row = (props: { key: string }) => {
+  return (
+    <tr>
+      <td>
+        {(props.key.length === 0 && (
+          <button type="button" id="submit">
+            Add
+          </button>
+        )) || (
+            <>
+              <button class="mb-2">Save</button>
+              <button>Delete</button>
+            </>
+          )}
+      </td>
+      <td>
+        <select autocomplete="off">
+          <option value="A">A</option>
+          <option value="B">B</option>
+          <option value="C">C</option>
+          <option value="D">D</option>
+          <option value="E">E</option>
+          <option value="F">F</option>
+          <option value="G">G</option>
+          <option value="H">H</option>
+          <option value="I">I</option>
+          <option value="J">J</option>
+          <option value="K">K</option>
+          <option value="L">L</option>
+          <option value="M">M</option>
+          <option value="N">N</option>
+          <option value="O">O</option>
+          <option value="P">P</option>
+          <option value="Q">Q</option>
+          <option value="R">R</option>
+          <option value="S">S</option>
+          <option value="T">T</option>
+          <option value="U">U</option>
+          <option value="V">V</option>
+          <option value="W">W</option>
+          <option value="X">X</option>
+          <option value="Y">Y</option>
+          <option value="Z">Z</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="0">0</option>
+          <option value="F1">F1</option>
+          <option value="F2">F2</option>
+          <option value="F3">F3</option>
+          <option value="F4">F4</option>
+          <option value="F5">F5</option>
+          <option value="F6">F6</option>
+          <option value="F7">F7</option>
+          <option value="F8">F8</option>
+          <option value="F9">F9</option>
+          <option value="F10">F10</option>
+          <option value="F11">F11</option>
+          <option value="F12">F12</option>
+        </select>
+      </td>
+      <td class="mods">
+        <div>
+          <input type="checkbox" autocomplete="off" />
+          Shift
+        </div>
+        <div>
+          <input type="checkbox" autocomplete="off" />
+          Ctrl
+        </div>
+        <div>
+          <input type="checkbox" autocomplete="off" />
+          Alt
+        </div>
+      </td>
+      <td>
+        <input class="sync" type="checkbox" autocomplete="off" />
+      </td>
+      <td class="size">-</td>
+      <td>
+        <textarea
+          class="code"
+          cols="40"
+          rows="5"
+          placeholder="Type code here"
+          autocomplete="off"
+          spellcheck={false}
+        />
+      </td>
+    </tr>
+  );
+};
+
+export default Row;
