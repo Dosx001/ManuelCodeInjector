ca230430597f178a771d2fde9db2633868da654e
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2026-08-19 13:09:29 -0700

    feat: add more key options to bindings

---
+++ src/options/components/Keys.tsx
@@ -0,0 +1,113 @@
+const Keys = () => {
+  return (
+    <>
+      <optgroup label="Letters">
+        <option value="A">A</option>
+        <option value="B">B</option>
+        <option value="C">C</option>
+        <option value="D">D</option>
+        <option value="E">E</option>
+        <option value="F">F</option>
+        <option value="G">G</option>
+        <option value="H">H</option>
+        <option value="I">I</option>
+        <option value="J">J</option>
+        <option value="K">K</option>
+        <option value="L">L</option>
+        <option value="M">M</option>
+        <option value="N">N</option>
+        <option value="O">O</option>
+        <option value="P">P</option>
+        <option value="Q">Q</option>
+        <option value="R">R</option>
+        <option value="S">S</option>
+        <option value="T">T</option>
+        <option value="U">U</option>
+        <option value="V">V</option>
+        <option value="W">W</option>
+        <option value="X">X</option>
+        <option value="Y">Y</option>
+        <option value="Z">Z</option>
+      </optgroup>
+      <optgroup label="Numbers">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="0">0</option>
+      </optgroup>
+      <optgroup label="Symbols">
+        <option value="Minus">-</option>
+        <option value="Equal">=</option>
+        <option value="BracketLeft">[</option>
+        <option value="BracketRight">]</option>
+        <option value="Backslash">\</option>
+        <option value="Semicolon">;</option>
+        <option value="Quote">'</option>
+        <option value="Comma">,</option>
+        <option value="Period">.</option>
+        <option value="Slash">/</option>
+        <option value="Backquote">`</option>
+      </optgroup>
+      <optgroup label="Function">
+        <option value="F1">F1</option>
+        <option value="F2">F2</option>
+        <option value="F3">F3</option>
+        <option value="F4">F4</option>
+        <option value="F5">F5</option>
+        <option value="F6">F6</option>
+        <option value="F7">F7</option>
+        <option value="F8">F8</option>
+        <option value="F9">F9</option>
+        <option value="F10">F10</option>
+        <option value="F11">F11</option>
+        <option value="F12">F12</option>
+      </optgroup>
+      <optgroup label="NumPad">
+        <option value="Numpad1">1</option>
+        <option value="Numpad2">2</option>
+        <option value="Numpad3">3</option>
+        <option value="Numpad4">4</option>
+        <option value="Numpad5">5</option>
+        <option value="Numpad6">6</option>
+        <option value="Numpad7">7</option>
+        <option value="Numpad8">8</option>
+        <option value="Numpad9">9</option>
+        <option value="Numpad0">0</option>
+        <option value="NumpadDecimal">.</option>
+        <option value="NumpadEnter">Enter</option>
+        <option value="NumpadAdd">+</option>
+        <option value="NumpadSubtract">-</option>
+        <option value="NumpadMultiply">*</option>
+        <option value="NumpadDivide">/</option>
+        <option value="NumpadEqual">=</option>
+      </optgroup>
+      <optgroup label="Arrows">
+        <option value="ArrowLeft">←</option>
+        <option value="ArrowRight">→</option>
+        <option value="ArrowUp">↑</option>
+        <option value="ArrowDown">↓</option>
+      </optgroup>
+      <optgroup label="Other">
+        <option value="Backspace">Backspace</option>
+        <option value="Delete">Delete</option>
+        <option value="Enter">Enter</option>
+        <option value="Escape">Escape</option>
+        <option value="Insert">Insert</option>
+        <option value="PageDown">PageDown</option>
+        <option value="PageUp">PageUp</option>
+        <option value="Pause">Pause</option>
+        <option value="PrintScreen">PrintScreen</option>
+        <option value="Space">Space</option>
+        <option value="Tab">Tab</option>
+      </optgroup>
+    </>
+  );
+};
+
+export default Keys;
--- src/options/components/Row.tsx
+++ src/options/components/Row.tsx
@@ -1,5 +1,6 @@
 import { Accessor, onMount, Setter, Show } from "solid-js";
 import { drag, safe, setDrag, setSafe } from "../App";
+import Keys from "./Keys";
 
 const Row = (props: {
   index: number;
@@ -180,54 +181,7 @@ const Row = (props: {
           class="cursor-pointer rounded border border-[#484848] bg-black text-[darkgray]"
           onChange={disable}
         >
-          <option value="A">A</option>
-          <option value="B">B</option>
-          <option value="C">C</option>
-          <option value="D">D</option>
-          <option value="E">E</option>
-          <option value="F">F</option>
-          <option value="G">G</option>
-          <option value="H">H</option>
-          <option value="I">I</option>
-          <option value="J">J</option>
-          <option value="K">K</option>
-          <option value="L">L</option>
-          <option value="M">M</option>
-          <option value="N">N</option>
-          <option value="O">O</option>
-          <option value="P">P</option>
-          <option value="Q">Q</option>
-          <option value="R">R</option>
-          <option value="S">S</option>
-          <option value="T">T</option>
-          <option value="U">U</option>
-          <option value="V">V</option>
-          <option value="W">W</option>
-          <option value="X">X</option>
-          <option value="Y">Y</option>
-          <option value="Z">Z</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="0">0</option>
-          <option value="F1">F1</option>
-          <option value="F2">F2</option>
-          <option value="F3">F3</option>
-          <option value="F4">F4</option>
-          <option value="F5">F5</option>
-          <option value="F6">F6</option>
-          <option value="F7">F7</option>
-          <option value="F8">F8</option>
-          <option value="F9">F9</option>
-          <option value="F10">F10</option>
-          <option value="F11">F11</option>
-          <option value="F12">F12</option>
+          <Keys />
         </select>
       </td>
       <td ref={mods} class="w-16 text-left">
